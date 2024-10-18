5a56ef2365f8c945ad7c6038a171a3ab315d62bb
WardDeb <ward@deboutte.be>
2024-10-18 12:20:43 +0200

    initial maturin structure

---
+++ pydeeptools/deeptools/bamCoverage2.py
@@ -0,0 +1,135 @@
+import argparse
+from deeptools import parserCommon
+from deeptools.hp import r_bamcoverage
+
+
+def parseArguments():
+    parentParser = parserCommon.getParentArgParse()
+    bamParser = parserCommon.read_options()
+    normalizationParser = parserCommon.normalization_options()
+    requiredArgs = get_required_args()
+    optionalArgs = get_optional_args()
+    outputParser = parserCommon.output()
+    parser = \
+        argparse.ArgumentParser(
+            parents=[requiredArgs, outputParser, optionalArgs,
+                     parentParser, normalizationParser, bamParser],
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description='This tool takes an alignment of reads or fragments '
+            'as input (BAM file) and generates a coverage track (bigWig or '
+            'bedGraph) as output. '
+            'The coverage is calculated as the number of reads per bin, '
+            'where bins are short consecutive counting windows of a defined '
+            'size. It is possible to extended the length of the reads '
+            'to better reflect the actual fragment length. *bamCoverage* '
+            'offers normalization by scaling factor, Reads Per Kilobase per '
+            'Million mapped reads (RPKM), counts per million (CPM), bins per '
+            'million mapped reads (BPM) and 1x depth (reads per genome '
+            'coverage, RPGC).\n',
+            usage='bamCoverage -b reads.bam -o coverage.bw\n'
+            'help: bamCoverage -h / bamCoverage --help',
+            add_help=False)
+
+    return parser
+
+
+def get_required_args():
+    parser = argparse.ArgumentParser(add_help=False)
+
+    required = parser.add_argument_group('Required arguments')
+
+    # define the arguments
+    required.add_argument('--bam', '-b',
+                          help='BAM file to process',
+                          metavar='BAM file',
+                          required=True)
+
+    return parser
+
+
+def get_optional_args():
+
+    parser = argparse.ArgumentParser(add_help=False)
+    optional = parser.add_argument_group('Optional arguments')
+
+    optional.add_argument("--help", "-h", action="help",
+                          help="show this help message and exit")
+
+    optional.add_argument('--scaleFactor',
+                          help='The computed scaling factor (or 1, if not applicable) will '
+                          'be multiplied by this. (Default: %(default)s)',
+                          default=1.0,
+                          type=float,
+                          required=False)
+
+    optional.add_argument('--MNase',
+                          help='Determine nucleosome positions from MNase-seq data. '
+                          'Only 3 nucleotides at the center of each fragment are counted. '
+                          'The fragment ends are defined by the two mate reads. Only fragment lengths'
+                          'between 130 - 200 bp are considered to avoid dinucleosomes or other artifacts. '
+                          'By default, any fragments smaller or larger than this are ignored. To '
+                          'over-ride this, use the --minFragmentLength and --maxFragmentLength options, '
+                          'which will default to 130 and 200 if not otherwise specified in the presence '
+                          'of --MNase. *NOTE*: Requires paired-end data. A bin size of 1 is recommended.',
+                          action='store_true')
+
+    optional.add_argument('--Offset',
+                          help='Uses this offset inside of each read as the signal. This is useful in '
+                          'cases like RiboSeq or GROseq, where the signal is 12, 15 or 0 bases past the '
+                          'start of the read. This can be paired with the --filterRNAstrand option. '
+                          'Note that negative values indicate offsets from the end of each read. A value '
+                          'of 1 indicates the first base of the alignment (taking alignment orientation '
+                          'into account). Likewise, a value of -1 is the last base of the alignment. An '
+                          'offset of 0 is not permitted. If two values are specified, then they will be '
+                          'used to specify a range of positions. Note that specifying something like '
+                          '--Offset 5 -1 will result in the 5th through last position being used, which '
+                          'is equivalent to trimming 4 bases from the 5-prime end of alignments. Note '
+                          'that if you specify --centerReads, the centering will be performed before the '
+                          'offset.',
+                          metavar='INT',
+                          type=int,
+                          nargs='+',
+                          required=False)
+
+    optional.add_argument('--filterRNAstrand',
+                          help='Selects RNA-seq reads (single-end or paired-end) originating from genes '
+                          'on the given strand. This option assumes a standard dUTP-based library '
+                          'preparation (that is, --filterRNAstrand=forward keeps minus-strand reads, '
+                          'which originally came from genes on the forward strand using a dUTP-based '
+                          'method). Consider using --samExcludeFlag instead for filtering by strand in '
+                          'other contexts.',
+                          choices=['forward', 'reverse'],
+                          default=None)
+
+    return parser
+
+def scaleFactor(string):
+    try:
+        scalefactor1, scalefactor2 = string.split(":")
+        scalefactors = (float(scalefactor1), float(scalefactor2))
+    except:
+        raise argparse.ArgumentTypeError(
+            "Format of scaleFactors is factor1:factor2. "
+            "The value given ( {} ) is not valid".format(string))
+
+    return scalefactors
+
+
+def process_args(args=None):
+    args = parseArguments().parse_args(args)
+
+    if args.smoothLength and args.smoothLength <= args.binSize:
+        print("Warning: the smooth length given ({}) is smaller than the bin "
+              "size ({}).\n\n No smoothing will be done".format(args.smoothLength, args.binSize))
+        args.smoothLength = None
+
+    if not args.ignoreForNormalization:
+        args.ignoreForNormalization = []
+
+    return args
+
+def main(args=None):
+    args = process_args(args)
+    test = r_bamcoverage(args.bam, args.outFileName)
+    
+
