be59d7efc86a69afb57ad8e511fbf1f2d0e03eff
WardDeb <ward@deboutte.be>
2024-11-20 09:44:13 +0100

    move writing bedgraph/bigwig file to filehandler

--- src/bamcoverage.rs
+++ src/bamcoverage.rs
@@ -1,12 +1,8 @@
 use pyo3::prelude::*;
 use rayon::prelude::*;
 use rayon::ThreadPoolBuilder;
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use bigtools::{BigWigWrite, Value};
-use bigtools::beddata::BedParserStreamingIterator;
 use crate::covcalc::{bam_pileup, parse_regions};
-use crate::filehandler::bam_stats;
+use crate::filehandler::{bam_stats, write_file};
 use crate::normalization::scale_factor;
 
 #[pyfunction]
@@ -42,27 +38,6 @@ pub fn r_bamcoverage(
     });
 
     // Create output
-    if ofiletype == "bedgraph" {
-        // write output file, bedgraph
-        let mut writer = BufWriter::new(File::create(ofile).unwrap());
-        for i in _bg {
-            writeln!(writer, "{}\t{}\t{}\t{}", i.0, i.1, i.2, i.3).unwrap();
-        }
-    } else {
-        // write output file, bigwig
-        let vals = BedParserStreamingIterator::wrap_infallible_iter(
-            _bg.iter().map(
-                |(chr, start, end, cov)| {(chr.as_str(), Value {start: *start as u32, end: *end as u32, value: *cov as f32 } )}
-            ),
-            true
-        );
-        // Theoretically one could add more threads here too, but this would require rewrite of the _bg iter upstream.
-        let runtime = tokio::runtime::Builder::new_multi_thread()
-            .worker_threads(1)
-            .build()
-            .expect("Unable to create tokio runtime for bw writing.");
-        let writer = BigWigWrite::create_file(ofile, chromsizes).unwrap();
-        let _ = writer.write(vals, runtime);
-    }
+    write_file(ofile, ofiletype, _bg, chromsizes).unwrap();
     Ok(())
 }
