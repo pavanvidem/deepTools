73733046197e92b480f4cf0784552b10b4245211
WardDeb <ward@deboutte.be>
2024-11-29 17:48:24 +0100

    WIP for (gentle?) tmp files per chrom

--- src/bamcompare.rs
+++ src/bamcompare.rs
@@ -1,25 +1,36 @@
 use pyo3::prelude::*;
+use pyo3::types::PyList;
 use rayon::prelude::*;
 use rayon::ThreadPoolBuilder;
 use crate::filehandler::{bam_ispaired, write_file};
-use crate::covcalc::{bam_pileup, parse_regions, collapse_bgvecs};
+use crate::covcalc::{bam_pileup, parse_regions, alignmentfilters};
 use crate::normalization::scale_factor_bamcompare;
 use crate::calc::median;
 
 #[pyfunction]
 pub fn r_bamcompare(
-    bam_ifile1: &str,
-    bam_ifile2: &str,
-    ofile: &str,
-    ofiletype: &str,
+    // input and output
+    bam_ifile1: &str, // input bamfile 1
+    bam_ifile2: &str, // input bamfile 2
+    ofile: &str, // output file
+    ofiletype: &str, // ouput file type, bedgraph or bigwig
+    // norm options
     norm: &str,
-    scalefactorsmethod: &str,
     effective_genome_size: u64,
+    scalefactorsmethod: &str,
     operation: &str,
-    pseudocount: f64,
+    pseudocount: f32,
+    // filtering options
+    ignoreduplicates: bool,
+    minmappingquality: u8, // 
+    samflaginclude: u16,
+    samflagexclude: u16,
+    minfraglen: u32,
+    maxfraglen: u32,
     nproc: usize,
+    _ignorechr: Py<PyList>,
     binsize: u32,
-    regions: Vec<(String, u64, u64)>,
+    regions: Vec<(String, u32, u32)>,
     verbose: bool
 ) -> PyResult<()> {
     let ispe1 = bam_ispaired(bam_ifile1);
@@ -29,51 +40,63 @@ pub fn r_bamcompare(
         println!("Sample1: {} is-paired: {}", bam_ifile1, ispe1);
         println!("Sample2: {} is-paired: {}", bam_ifile2, ispe2);
     }
+    let mut ignorechr: Vec<String> = Vec::new();
+    Python::with_gil(|py| {
+        ignorechr = _ignorechr.extract(py).expect("Failed to retrieve ignorechr.");
+    });
+    // Set alignment filters
+    let filters = alignmentfilters {
+        minmappingquality: minmappingquality,
+        samflaginclude: samflaginclude,
+        samflagexclude: samflagexclude,
+        minfraglen: minfraglen,
+        maxfraglen: maxfraglen
+    };
 
     // Parse regions & calculate coverage
     let (regions, chromsizes)  = parse_regions(&regions, bam_ifile1);
     let pool = ThreadPoolBuilder::new().num_threads(nproc).build().unwrap();
     
-    // Parse first bamfile
-    let (bg1, mapped1, _unmapped1, readlen1, fraglen1) = pool.install(|| {
-        regions.par_iter()
-            .map(|i| bam_pileup(bam_ifile1, &i, &binsize, &ispe1))
-            .reduce(
-                || (vec![], 0, 0, vec![], vec![]),
-                |(mut _bg, mut _mapped, mut _unmapped, mut _readlen, mut _fraglen), (bg, mapped, unmapped, readlen, fraglen)| {
-                    _bg.extend(bg);
-                    _readlen.extend(readlen);
-                    _fraglen.extend(fraglen);
-                    _mapped += mapped;
-                    _unmapped += unmapped;
-                    (_bg, _mapped, _unmapped, _readlen, _fraglen)
-                }
-            )
-    });
-    let _readlen1 = median(readlen1);
-    let _fraglen1 = median(fraglen1);
+    // // Parse first bamfile
+    // let (bg1, mapped1, _unmapped1, readlen1, fraglen1) = pool.install(|| {
+    //     regions.par_iter()
+    //         .map(|i| bam_pileup(bam_ifile1, &i, &binsize, &ispe1, &ignorechr, &filters))
+    //         .reduce(
+    //             || (vec![], 0, 0, vec![], vec![]),
+    //             |(mut _bg, mut _mapped, mut _unmapped, mut _readlen, mut _fraglen), (bg, mapped, unmapped, readlen, fraglen)| {
+    //                 _bg.extend(bg);
+    //                 _readlen.extend(readlen);
+    //                 _fraglen.extend(fraglen);
+    //                 _mapped += mapped;
+    //                 _unmapped += unmapped;
+    //                 (_bg, _mapped, _unmapped, _readlen, _fraglen)
+    //             }
+    //         )
+    // });
+    // let _readlen1 = median(readlen1);
+    // let _fraglen1 = median(fraglen1);
 
-    // Parse first bamfile
-    let (bg2, mapped2, _unmapped2, readlen2, fraglen2) = pool.install(|| {
-        regions.par_iter()
-            .map(|i| bam_pileup(bam_ifile2, &i, &binsize, &ispe2))
-            .reduce(
-                || (vec![], 0, 0, vec![], vec![]),
-                |(mut _bg, mut _mapped, mut _unmapped, mut _readlen, mut _fraglen), (bg, mapped, unmapped, readlen, fraglen)| {
-                    _bg.extend(bg);
-                    _readlen.extend(readlen);
-                    _fraglen.extend(fraglen);
-                    _mapped += mapped;
-                    _unmapped += unmapped;
-                    (_bg, _mapped, _unmapped, _readlen, _fraglen)
-                }
-            )
-    });
-    let _readlen2 = median(readlen2);
-    let _fraglen2 = median(fraglen2);
-    let (sf1, sf2) = scale_factor_bamcompare(scalefactorsmethod, mapped1, mapped2, binsize, effective_genome_size, norm);
-    println!("scale factor1 = {}, scale factor2 = {}", sf1, sf2);
-    let bge = collapse_bgvecs(bg1, bg2, sf1, sf2, pseudocount, operation);
-    write_file(ofile, ofiletype, bge, chromsizes);
+    // // Parse second bamfile
+    // let (bg2, mapped2, _unmapped2, readlen2, fraglen2) = pool.install(|| {
+    //     regions.par_iter()
+    //         .map(|i| bam_pileup(bam_ifile2, &i, &binsize, &ispe2, &ignorechr, &filters))
+    //         .reduce(
+    //             || (vec![], 0, 0, vec![], vec![]),
+    //             |(mut _bg, mut _mapped, mut _unmapped, mut _readlen, mut _fraglen), (bg, mapped, unmapped, readlen, fraglen)| {
+    //                 _bg.extend(bg);
+    //                 _readlen.extend(readlen);
+    //                 _fraglen.extend(fraglen);
+    //                 _mapped += mapped;
+    //                 _unmapped += unmapped;
+    //                 (_bg, _mapped, _unmapped, _readlen, _fraglen)
+    //             }
+    //         )
+    // });
+    // let _readlen2 = median(readlen2);
+    // let _fraglen2 = median(fraglen2);
+    // let (sf1, sf2) = scale_factor_bamcompare(scalefactorsmethod, mapped1, mapped2, binsize, effective_genome_size, norm);
+    // println!("scale factor1 = {}, scale factor2 = {}", sf1, sf2);
+    // let bge = collapse_bgvecs(bg1, bg2, sf1, sf2, pseudocount, operation);
+    // write_file(ofile, ofiletype, bge, chromsizes);
     Ok(())
 }
--- src/filehandler.rs
+++ src/filehandler.rs
@@ -1,3 +1,4 @@
+use bigtools::utils::misc::Name;
 use rust_htslib::bam::{Read, Reader};
 use itertools::Itertools;
 use std::io::{BufReader, BufWriter, Write};
@@ -6,10 +7,12 @@ use std::fs::File;
 use std::path::Path;
 use bigtools::{BigWigRead, BigWigWrite, Value};
 use bigtools::beddata::BedParserStreamingIterator;
+use bigtools::bed::bedparser::parse_bedgraph;
 use std::collections::HashMap;
-use crate::computematrix::Scalingregions;
 use flate2::write::GzEncoder;
 use flate2::Compression;
+use tempfile::{TempPath};
+//use crate::computematrix::Scalingregions;
 
 pub fn bam_ispaired(bam_ifile: &str) -> bool {
     let mut bam = Reader::from_path(bam_ifile).unwrap();
@@ -22,22 +25,35 @@ pub fn bam_ispaired(bam_ifile: &str) -> bool {
     return false;
 }
 
-pub fn write_file(ofile: &str, filetype: &str, bg: Vec<(String, u64, u64, f64)>, chromsizes: HashMap<String, u32>) {
+pub fn write_file(ofile: &str, filetype: &str, bgs: Vec<TempPath>, chromsizes: HashMap<String, u32>, sf: f32) {
+    // Create iterator over bg files
+    let lines = bgs.into_iter().flat_map(
+        |bg| {
+            let reader = BufReader::new(File::open(bg).unwrap());
+            reader.lines().map(
+                |l| {
+                    let l = l.unwrap();
+                    let fields: Vec<&str> = l.split('\t').collect();
+                    let chrom: String = fields[0].to_string();
+                    let start: u32 = fields[1].parse().unwrap();
+                    let end: u32 = fields[2].parse().unwrap();
+                    let cov: f32 = fields[3].parse().unwrap();
+                    (chrom, Value {start: start, end: end, value: cov * sf})
+                }
+            )
+        }
+    );
     if filetype == "bedgraph" {
         // write output file, bedgraph
         let mut writer = BufWriter::new(File::create(ofile).unwrap());
-        for i in bg {
-            writeln!(writer, "{}\t{}\t{}\t{}", i.0, i.1, i.2, i.3).unwrap();
+        for (chrom, val) in lines {
+            writeln!(writer, "{}\t{}\t{}\t{}", chrom, val.start, val.end, val.value).unwrap();
         }
     } else {
-        // write output file, bigwig
         let vals = BedParserStreamingIterator::wrap_infallible_iter(
-            bg.iter().map(
-                |(chr, start, end, cov)| {(chr.as_str(), Value {start: *start as u32, end: *end as u32, value: *cov as f32 } )}
-            ),
+            lines,
             false
         );
-        // Theoretically one could add more threads here too, but this would require rewrite of the _bg iter upstream.
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .worker_threads(1)
             .build()
@@ -47,13 +63,14 @@ pub fn write_file(ofile: &str, filetype: &str, bg: Vec<(String, u64, u64, f64)>,
         println!("Start writer");
         let _ = writer.write(vals, runtime);
     }
+
 }
 
-pub fn read_bedfiles(bed_files: &Vec<String>) -> (Vec<(String, u64, u64, String, String, String)>, HashMap<String, u64>) {
+pub fn read_bedfiles(bed_files: &Vec<String>) -> (Vec<(String, u32, u32, String, String, String)>, HashMap<String, u32>) {
     // read all bedfiles in a Vec of strings (filepaths)
     // returns a vec of tuples, with last entry being the filepath (without extension and dirs ~= 'smartLabels')
-    let mut regionsizes: HashMap<String, u64> = HashMap::new();
-    let mut regions: Vec<(String, u64, u64, String, String, String)> = Vec::new();
+    let mut regionsizes: HashMap<String, u32> = HashMap::new();
+    let mut regions: Vec<(String, u32, u32, String, String, String)> = Vec::new();
     for bed in bed_files {
         let entryname = Path::new(bed)
             .file_stem()
@@ -62,7 +79,7 @@ pub fn read_bedfiles(bed_files: &Vec<String>) -> (Vec<(String, u64, u64, String,
             .into_owned();
 
         let bedfile = BufReader::new(File::open(bed).unwrap());
-        let mut entries: u64 = 0;
+        let mut entries: u32 = 0;
         for line in bedfile.lines() {
             let line = line.unwrap();
             let fields: Vec<&str> = line.split('\t').collect();
@@ -97,155 +114,155 @@ pub fn chrombounds_from_bw(bwfile: &str) -> HashMap<String, u64> {
     chromsizes
 }
 
-pub fn bwintervals(
-    bwfile: &str,
-    regions: &Vec<(String, u64, u64, String, String, String)>,
-    slopregions: &Vec<Vec<(u64, u64)>>,
-    scale_regions: &Scalingregions
-) -> Vec<Vec<f64>> {
-    // For a given bw file, a vector of slop regions (where every vec entry is a vec of (start, end) tuples)
-    // return a vector with for every region a vector of f64.
+// pub fn bwintervals(
+//     bwfile: &str,
+//     regions: &Vec<(String, u64, u64, String, String, String)>,
+//     slopregions: &Vec<Vec<(u64, u64)>>,
+//     scale_regions: &Scalingregions
+// ) -> Vec<Vec<f64>> {
+//     // For a given bw file, a vector of slop regions (where every vec entry is a vec of (start, end) tuples)
+//     // return a vector with for every region a vector of f64.
 
-    // Make sure regions and slopregions are of equal length
-    assert_eq!(regions.len(), slopregions.len());
+//     // Make sure regions and slopregions are of equal length
+//     assert_eq!(regions.len(), slopregions.len());
     
-    // Define return vector, set up bw reader.
-    let mut bwvals: Vec<Vec<f64>> = Vec::new();
-    let bwf = File::open(bwfile).expect("Failed to open bw file.");
-    let mut reader = BigWigRead::open(bwf).unwrap();
+//     // Define return vector, set up bw reader.
+//     let mut bwvals: Vec<Vec<f64>> = Vec::new();
+//     let bwf = File::open(bwfile).expect("Failed to open bw file.");
+//     let mut reader = BigWigRead::open(bwf).unwrap();
 
-    // Iterate over regions and slopregions synchronously.
-    for (sls, (chrom, _,  _, _, _, _)) in slopregions.iter().zip(regions.iter()) {
-        let mut bwval: Vec<f64> = Vec::new();
-        // get 'min and max' to query
-        let (min, max) = sls
-            .iter()
-            .flat_map(|(x, y)| vec![*x, *y])
-            .filter(|&value| value != 0)
-            .minmax()
-            .into_option()
-            .expect("Vec is empty.");
-        let binvals = reader.get_interval(chrom, min as u32, max as u32).unwrap();
-        // since binvals (can) be over binsizes, we expand them to bp and push them to a hashmap
-        let mut bwhash: HashMap<u64, f64> = HashMap::new();
-        for interval in binvals {
-            let interval = interval.unwrap();
-            let start = interval.start as u64;
-            let end = interval.end as u64;
-            let val = interval.value as f64;
-            bwhash.extend((start..end).map(|bp| (bp, val)));
-        }
-        for (start, end) in sls {
-            if start == end && *end == 0 {
-                if scale_regions.missingdata_as_zero {
-                    bwval.push(0.0);
-                } else {
-                    bwval.push(std::f64::NAN);
-                }
-            } else {
-                // Get values from the hashmap
-                let mean: f64 = (*start..*end)
-                    .filter_map(|bp| bwhash.get(&bp))
-                    .copied()
-                    .sum::<f64>()
-                    / (end - start) as f64;
-                bwval.push(mean);
-            }
-        }
-        // Make sure bwval is of expected length.
-        assert_eq!(bwval.len(), scale_regions.cols_expected / scale_regions.bwfiles);
-        bwvals.push(bwval);
-    }
-    bwvals
-}
+//     // Iterate over regions and slopregions synchronously.
+//     for (sls, (chrom, _,  _, _, _, _)) in slopregions.iter().zip(regions.iter()) {
+//         let mut bwval: Vec<f64> = Vec::new();
+//         // get 'min and max' to query
+//         let (min, max) = sls
+//             .iter()
+//             .flat_map(|(x, y)| vec![*x, *y])
+//             .filter(|&value| value != 0)
+//             .minmax()
+//             .into_option()
+//             .expect("Vec is empty.");
+//         let binvals = reader.get_interval(chrom, min as u32, max as u32).unwrap();
+//         // since binvals (can) be over binsizes, we expand them to bp and push them to a hashmap
+//         let mut bwhash: HashMap<u64, f64> = HashMap::new();
+//         for interval in binvals {
+//             let interval = interval.unwrap();
+//             let start = interval.start as u64;
+//             let end = interval.end as u64;
+//             let val = interval.value as f64;
+//             bwhash.extend((start..end).map(|bp| (bp, val)));
+//         }
+//         for (start, end) in sls {
+//             if start == end && *end == 0 {
+//                 if scale_regions.missingdata_as_zero {
+//                     bwval.push(0.0);
+//                 } else {
+//                     bwval.push(std::f64::NAN);
+//                 }
+//             } else {
+//                 // Get values from the hashmap
+//                 let mean: f64 = (*start..*end)
+//                     .filter_map(|bp| bwhash.get(&bp))
+//                     .copied()
+//                     .sum::<f64>()
+//                     / (end - start) as f64;
+//                 bwval.push(mean);
+//             }
+//         }
+//         // Make sure bwval is of expected length.
+//         assert_eq!(bwval.len(), scale_regions.cols_expected / scale_regions.bwfiles);
+//         bwvals.push(bwval);
+//     }
+//     bwvals
+// }
 
-pub fn header_matrix(scale_regions: &Scalingregions, regionsizes: HashMap<String, u64>) -> String {
-    // Create the header for the matrix.
-    // This is quite ugly, but this is mainly because we need to accomodate delta bwfiles.
-    let mut headstr = String::new();
-    headstr.push_str("@{");
-    headstr.push_str(
-        &format!("\"upstream\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.upstream).collect::<Vec<_>>().into_iter().join(","))
-    );
-    headstr.push_str(
-        &format!("\"downstream\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.downstream).collect::<Vec<_>>().into_iter().join(","))
-    );
-    headstr.push_str(
-        &format!("\"body\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.regionbodylength).collect::<Vec<_>>().into_iter().join(","))
-    );
-    headstr.push_str(
-        &format!("\"bin size\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.binsize).collect::<Vec<_>>().into_iter().join(","))
-    );
-    headstr.push_str(
-        &format!("\"ref point\":[\"{}\"],", (0..scale_regions.bwfiles).map(|_| scale_regions.referencepoint.clone()).collect::<Vec<_>>().into_iter().join("\",\""))
-    );
-    headstr.push_str(
-        &format!("\"verbose\":{},", scale_regions.verbose)
-    );
-    headstr.push_str(
-        &format!("\"bin avg type\":\"{}\",", scale_regions.avgtype)
-    );
-    headstr.push_str(
-        &format!("\"missing data as zero\":{},", scale_regions.missingdata_as_zero)
-    );
-    // Unimplemented arguments, but they need to be present in header for now anyway.
-    headstr.push_str(
-        "\"min threshold\":null,\"max threshold\":null,\"scale\":1,\"skip zeros\":false,\"nan after end\":false,"
-    );
-    headstr.push_str(
-        &format!("\"proc number\":{},", scale_regions.proc_number)
-    );
-    // Unimplemented for now...
-    headstr.push_str(
-        "\"sort regions\":\"keep\",\"sort using\":\"mean\","
-    );
-    headstr.push_str(
-        &format!("\"unscaled 5 prime\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.unscaled5prime).collect::<Vec<_>>().into_iter().join(","))
-    );
-    headstr.push_str(
-        &format!("\"unscaled 3 prime\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.unscaled3prime).collect::<Vec<_>>().into_iter().join(","))
-    );
-    headstr.push_str(
-        &format!("\"group_labels\":[\"{}\"],", scale_regions.bedlabels.join("\",\""))
-    );
-    // Get cumulative sizes of regions
-    let mut groupbounds: Vec<u64> = Vec::new();
-    groupbounds.push(0);
-    let mut cumsum: u64 = 0;
-    for bedlabel in scale_regions.bedlabels.iter() {
-        cumsum += regionsizes.get(bedlabel).unwrap();
-        groupbounds.push(cumsum);
-    }
-    let groupbounds = format!("{}", groupbounds.iter()
-        .map(|&x| x.to_string())
-        .collect::<Vec<String>>()
-        .join(","));
+// pub fn header_matrix(scale_regions: &Scalingregions, regionsizes: HashMap<String, u64>) -> String {
+//     // Create the header for the matrix.
+//     // This is quite ugly, but this is mainly because we need to accomodate delta bwfiles.
+//     let mut headstr = String::new();
+//     headstr.push_str("@{");
+//     headstr.push_str(
+//         &format!("\"upstream\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.upstream).collect::<Vec<_>>().into_iter().join(","))
+//     );
+//     headstr.push_str(
+//         &format!("\"downstream\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.downstream).collect::<Vec<_>>().into_iter().join(","))
+//     );
+//     headstr.push_str(
+//         &format!("\"body\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.regionbodylength).collect::<Vec<_>>().into_iter().join(","))
+//     );
+//     headstr.push_str(
+//         &format!("\"bin size\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.binsize).collect::<Vec<_>>().into_iter().join(","))
+//     );
+//     headstr.push_str(
+//         &format!("\"ref point\":[\"{}\"],", (0..scale_regions.bwfiles).map(|_| scale_regions.referencepoint.clone()).collect::<Vec<_>>().into_iter().join("\",\""))
+//     );
+//     headstr.push_str(
+//         &format!("\"verbose\":{},", scale_regions.verbose)
+//     );
+//     headstr.push_str(
+//         &format!("\"bin avg type\":\"{}\",", scale_regions.avgtype)
+//     );
+//     headstr.push_str(
+//         &format!("\"missing data as zero\":{},", scale_regions.missingdata_as_zero)
+//     );
+//     // Unimplemented arguments, but they need to be present in header for now anyway.
+//     headstr.push_str(
+//         "\"min threshold\":null,\"max threshold\":null,\"scale\":1,\"skip zeros\":false,\"nan after end\":false,"
+//     );
+//     headstr.push_str(
+//         &format!("\"proc number\":{},", scale_regions.proc_number)
+//     );
+//     // Unimplemented for now...
+//     headstr.push_str(
+//         "\"sort regions\":\"keep\",\"sort using\":\"mean\","
+//     );
+//     headstr.push_str(
+//         &format!("\"unscaled 5 prime\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.unscaled5prime).collect::<Vec<_>>().into_iter().join(","))
+//     );
+//     headstr.push_str(
+//         &format!("\"unscaled 3 prime\":[{}],", (0..scale_regions.bwfiles).map(|_| scale_regions.unscaled3prime).collect::<Vec<_>>().into_iter().join(","))
+//     );
+//     headstr.push_str(
+//         &format!("\"group_labels\":[\"{}\"],", scale_regions.bedlabels.join("\",\""))
+//     );
+//     // Get cumulative sizes of regions
+//     let mut groupbounds: Vec<u64> = Vec::new();
+//     groupbounds.push(0);
+//     let mut cumsum: u64 = 0;
+//     for bedlabel in scale_regions.bedlabels.iter() {
+//         cumsum += regionsizes.get(bedlabel).unwrap();
+//         groupbounds.push(cumsum);
+//     }
+//     let groupbounds = format!("{}", groupbounds.iter()
+//         .map(|&x| x.to_string())
+//         .collect::<Vec<String>>()
+//         .join(","));
 
-    headstr.push_str(
-        &format!("\"group_boundaries\":[{}],", groupbounds)
-    );
-    // Get cumulative sizes for sample boundaries
-    let colsize_per_sample = scale_regions.cols_expected / scale_regions.bwfiles;
-    let mut samplebounds: Vec<u64> = Vec::new();
-    samplebounds.push(0);
-    let mut cumsum: u64 = 0;
-    for i in 0..scale_regions.bwfiles {
-        cumsum += colsize_per_sample as u64;
-        samplebounds.push(cumsum);
-    }
-    let samplebounds = format!("{}", samplebounds.iter()
-        .map(|&x| x.to_string())
-        .collect::<Vec<String>>()
-        .join(","));
+//     headstr.push_str(
+//         &format!("\"group_boundaries\":[{}],", groupbounds)
+//     );
+//     // Get cumulative sizes for sample boundaries
+//     let colsize_per_sample = scale_regions.cols_expected / scale_regions.bwfiles;
+//     let mut samplebounds: Vec<u64> = Vec::new();
+//     samplebounds.push(0);
+//     let mut cumsum: u64 = 0;
+//     for i in 0..scale_regions.bwfiles {
+//         cumsum += colsize_per_sample as u64;
+//         samplebounds.push(cumsum);
+//     }
+//     let samplebounds = format!("{}", samplebounds.iter()
+//         .map(|&x| x.to_string())
+//         .collect::<Vec<String>>()
+//         .join(","));
 
-    headstr.push_str(
-        &format!("\"sample_boundaries\":[{}]", samplebounds)
-    );
-    headstr.push_str(
-        "}\n"
-    );
-    headstr
-}
+//     headstr.push_str(
+//         &format!("\"sample_boundaries\":[{}]", samplebounds)
+//     );
+//     headstr.push_str(
+//         "}\n"
+//     );
+//     headstr
+// }
 
 pub fn write_matrix(header: String, mat: Vec<Vec<f64>>, ofile: &str, regions: Vec<(String, u64, u64, String, String, String)>) {
     println!("Writing out matrix to file.");
